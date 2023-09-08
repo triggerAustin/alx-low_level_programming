1e5da042f229726e3c77231f7fba3f4efb604065
BigGuy <austinmuthoni0101@gmail.com>
2023-09-08 10:46:09 -0700

    101-print_comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main- start of prgoram
+ * @void: main return type
+ *
+ * Description: prints combination of 3 digit numbers
+ * Return: 0 is run success
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 0; i <= 7; i++)
+	{
+		for (j = i + 1; j <= 8; j++)
+		{
+			for (k = j + 1; k <= 9; k++)
+			{
+				putchar('0' + i);
+				putchar('0' + j);
+				putchar('0' + k);
+
+				if (i != 7 || j != 8 || k != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
