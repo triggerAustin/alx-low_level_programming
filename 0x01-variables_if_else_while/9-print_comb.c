537013967c46ae7b70d0d54c6c7ffe14ea58168d
BigGuy <austinmuthoni0101@gmail.com>
2023-09-08 09:51:59 -0700

    printcomb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -16,8 +16,14 @@ int main(void)
 		putchar(i + '0');
 		if (i < 9)
 		{
-			putchar(',');
-			putchar(' ');
+			int j;
+			for (j = 44; j >31; j--)
+			{
+				if (j == 32 || j == 44)
+				{
+					putchar(j);
+				}
+			}	
 		}
 		else
 		{
