2e2b4d1cb93f145eb7c18077c7fb82f05d6517d0
BigGuy <austinmuthoni0101@gmail.com>
2023-09-08 08:44:36 -0700

    7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - start of program
+ * @void: return type for main
+ *
+ * Description: prints all lower case alphabets
+ * Return: 0 is run successfully
+ */
+int main(void)
+{
+	char c; /*initialize char c*/
+
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
