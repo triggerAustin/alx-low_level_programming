42587d9ccd6576a622ec8294626ad91da215d032
BigGuy <austinmuthoni0101@gmail.com>
2023-09-08 08:17:44 -0700

    3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - start of program
+ * @void: return type for main
+ *
+ * Description: prints all lower case alphabets
+ * Return: 0 is run successfully
+ */
+int main(void)
+{
+	char c; /*initialize char c*/
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
