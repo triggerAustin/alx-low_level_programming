196dfc8cc8b66c3331922aa924c952e6daac50db
BigGuy <austinmuthoni0101@gmail.com>
2023-09-08 04:42:48 -0700

    put4c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
- /*prints a string*/
+
+/**
+ * This is a simple C program that prints a message.
+ *
+ * @return 0 on successful execution.
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	return(0);
 }
