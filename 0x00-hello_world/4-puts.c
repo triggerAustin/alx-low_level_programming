ab0fa44dca28b40b114ca1fc44f1cf395076a886
BigGuy <austinmuthoni0101@gmail.com>
2023-09-08 04:56:54 -0700

    full description

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,10 @@
 
 /**
  * main - puts a simple message
+ * @void - return  type is void
  *
- * Description: This is a simple C program that prints a message using the puts function
+ * Description: This is a simple C program
+ * it prints a message using the puts function
  * Return 0 on successful execution.
  */
 int main(void)
