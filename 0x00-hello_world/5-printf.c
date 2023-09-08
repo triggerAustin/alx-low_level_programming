01bd8e5359391d6c3afbd9b33804dc83f58ba625
BigGuy <austinmuthoni0101@gmail.com>
2023-09-08 05:06:44 -0700

    5-printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - printf message to console
+ * @void: return type of main
+ *
+ * Description: uses printf to output msg
+ * Return: 0 when execute successfully
+ */
+int main (void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
