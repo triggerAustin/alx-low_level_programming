202f9f44e6db284e573f373f0c3fd2063a1682a7
BigGuy <austinmuthoni0101@gmail.com>
2023-09-08 08:57:30 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - start of program
+ * @void: main return type
+ *
+ * Description: prints hexadecimal num
+ * Return: 0 if run successful
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 16; i++)
+	{
+		if (i < 10)
+		{
+			putchar('0' + i);
+		}
+		else
+		{
+			putchar('A' + (i - 10));
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
