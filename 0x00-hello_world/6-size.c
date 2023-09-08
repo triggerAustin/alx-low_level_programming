3fbd3b9d811596e21deff88d1eebcd5b79dc74cd
BigGuy <austinmuthoni0101@gmail.com>
2023-09-08 05:45:37 -0700

    size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - start of program
+ * @void: return type
+ * 
+ * Description: prints size of data types
+ * Return: 0 when run sucessfully
+ */
+int main(void)
+{
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of float: %lu byte(s)\n", sizeof(float));
+	return (0);
+}
