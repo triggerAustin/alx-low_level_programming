f49423af252860a43e3aabdd0932c713aaa200d5
BigGuy <austinmuthoni0101@gmail.com>
2023-09-08 08:23:02 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - start of program
+ * @void: return type for main
+ *
+ * Description: prints all lower case alphabets
+ * Return: 0 is run successfully
+ */
+int main(void)
+{
+	char c; /*initialize char c*/
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c == 'e' || c == 'q')
+			continue;
+
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
