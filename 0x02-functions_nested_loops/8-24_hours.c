1b19e2ab50f591dbffd33b0d4587df1f9cc2106e
BigGuy <austinmuthoni0101@gmail.com>
2023-09-12 10:32:05 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * jack_bauer - prints time
+ * @void: no parameter
+ *
+ */
+void jack_bauer(void)
+{
+	int h1;
+	int h2;
+	int m1;
+	int m2;
+
+	for (h1 = 0; h1 < 3; h1++)
+	{
+		for (h2 = 0; h2 <= 4; h2++)
+		{
+			for (m1 = 0; m1 < 6; m1++)
+			{
+				for (m2 = 0; m2 <= 9; m2++)
+				{
+					_putchar('0' + h1);
+					_putchar('0' + h2);
+					_putchar(':');
+					_putchar('0' + m1);
+					_putchar('0' + m2);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
+
