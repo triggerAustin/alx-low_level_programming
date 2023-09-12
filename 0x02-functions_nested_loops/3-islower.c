51b212da7fe9a87baac76b529d95ab9f083582e4
BigGuy <austinmuthoni0101@gmail.com>
2023-09-12 07:32:38 -0700

    3-islower.c 5

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  *_islower - checks whether a value is lowercase
- * @int c: takes in an integer value to work on
- * 
+ * @c: takes in an integer value to work on
+ *
  * Return: the value of a
  */
 int _islower(int c)
