9db972db85374b8b2ce29c72f7ee64975ea06234
BigGuy <austinmuthoni0101@gmail.com>
2023-09-12 04:38:09 -0700

    _putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,6 @@
-#include <unistd.h>
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * _putchar: Prints characters
- */
-int _putchar(char c)
-{
-    return write(1, &c, 1);
-}
+int _putchar(char c);
+
+#endif /* MAIN_H */
