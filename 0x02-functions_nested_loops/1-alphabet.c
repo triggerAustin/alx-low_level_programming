b6df28c99b01245706826a434fca26076a542f00
BigGuy <austinmuthoni0101@gmail.com>
2023-09-12 05:58:44 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,15 +3,16 @@
  * print_alphabet - prints alphabets in lower case
  * @void: return type
  */
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
-	char c = 'a';
-
-	while (c <= 'z')
+	char c;
+	int i;
+	for (i = 1; i <= 10; i++)
 	{
-		_putchar(c);
-		c++;
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n')
 	}
-	_putchar('\n');
-
 }
