a810a4fc17e94bc78480787d8c978ff51f354470
BigGuy <austinmuthoni0101@gmail.com>
2023-09-12 10:41:06 -0700

    8-24_hours.c 2

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,8 +13,12 @@ void jack_bauer(void)
 
 	for (h1 = 0; h1 < 3; h1++)
 	{
-		for (h2 = 0; h2 <= 4; h2++)
-		{
+		for (h2 = 0; h2 <= 9; h2++)
+		{	
+			if (h1 == 2 && h2 == 4)
+			{
+				break;
+			}
 			for (m1 = 0; m1 < 6; m1++)
 			{
 				for (m2 = 0; m2 <= 9; m2++)
