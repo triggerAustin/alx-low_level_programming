46fe04c48ae88f294991f1cb98a7353acbad92cc
BigGuy <austinmuthoni0101@gmail.com>
2023-09-12 04:26:19 -0700

    0-putchar.c2

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main -  start of programme
+ * @Void: main return type
+ *
+ * Return: 0 if sucssesful
+ */
+int main(void)
+{
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
+
+    return (0);
+}
