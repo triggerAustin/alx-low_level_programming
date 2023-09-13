16eea2d31c3da3f63f7bce3618bb118dce936abd
BigGuy <austinmuthoni0101@gmail.com>
2023-09-13 07:10:04 -0700

    11-print_to_98.c2

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,7 +9,7 @@
 void print_to_98(int n)
 {
 	int i;
-	
+
 	if (n < 98)
 	{
 		for (i = n; i <= 98; i++)
@@ -42,4 +42,7 @@ void print_to_98(int n)
 		}
 
 	}
+	else if (i == 98)
+	{
+		printf("%d\n", i);
 }
