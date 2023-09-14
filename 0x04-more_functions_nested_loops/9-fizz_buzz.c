afc7ee463259a41dab2e0af270e173f039441a1b
BigGuy <austinmuthoni0101@gmail.com>
2023-09-14 05:44:44 -0700

    fizz_buzz.

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
  * main - start of program
@@ -12,20 +11,24 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 100; i++)
+	for (i = 1; i <= 100; i++) /*loop thorught th enumbers 1 to 100*/
 	{
+			/*check if i is multiple of 3 or 5*/
 		if ((i % 3) == 0 && (i % 5) == 0)
 		{
 			printf("FizzBuzz ");
 		}
+			/*check if multiple of 3*/
 		else if ((i % 3) == 0)
 		{
 			printf("Fizz ");
 		}
+			/*check if multiple of 5*/
 		else if ((i % 5) == 0)
 		{
 			printf("Buzz ");
 		}
+			/*print other numbers*/
 		else
 		{
 			printf("%d ", i);
