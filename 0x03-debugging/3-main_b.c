d6d44ba45a6395843888ec1369130343da9b2ceb
BigGuy <austinmuthoni0101@gmail.com>
2023-09-14 07:58:32 -0700

    2-mains

--- 0x03-debugging/3-main_b.c
+++ 0x03-debugging/3-main_b.c
@@ -9,19 +9,15 @@
 
 int main(void)
 {
-	int month;
-	int day;
-	int year;
+	int month, day, year;
 
-	month = 2;
-	day = 29;
-	year = 2000;
+	month = 4;
+	day = 01;
+	year = 2020;
 
 	printf("Date: %02d/%02d/%04d\n", month, day, year);
-
 	day = convert_day(month, day);
 
 	print_remaining_days(month, day, year);
-
 	return (0);
-}
+}	
