2782606ff4ea1b725e80ddb990d6ebc8c4ed00b8
BigGuy <austinmuthoni0101@gmail.com>
2023-09-14 05:27:06 -0700

    9-fizz_buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -3,20 +3,12 @@
 
 /**
  * main - start of program
- * fizz_buzz - prints numbers from 1 to 100
- * multiples of 3 as fizz
- * multiples of 5 as buzz
- * @void: takes no argument
+ * @void: takes no argument.
  *
  * Return: 0 if success
  */
 
 int main(void)
-{
-	fizz_buzz();
-	return (0);
-}
-void fizz_buzz(void)
 {
 	int i;
 
@@ -28,7 +20,7 @@ void fizz_buzz(void)
 		}
 		else if ((i % 3) == 0)
 		{
-				printf("Fizz ");
+			printf("Fizz ");
 		}
 		else if ((i % 5) == 0)
 		{
@@ -40,4 +32,5 @@ void fizz_buzz(void)
 		}
 	}
 	printf("\n");
+	return (0);
 }
