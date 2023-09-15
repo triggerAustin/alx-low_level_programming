b4aafa75e2927961dca354ec6f6ac58dda26a842
BigGuy <austinmuthoni0101@gmail.com>
2023-09-14 22:06:34 -0700

    print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,7 +9,16 @@ void print_number(int n)
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		if (n == INT_MIN)
+		{
+			_putchar('2');
+			n %= 1000000000;
+			n = -n;
+		}
+		else
+		{
+			n = -n;
+		}
 	}
 
 	if (n / 10 != 0)
