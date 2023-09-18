9eab496243b9a61e627c606a6c94d5efbfa1f247
BigGuy <austinmuthoni0101@gmail.com>
2023-09-18 05:10:27 -0700

    puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,12 +2,13 @@
 
 /**
  * _puts - prints out a given string
- * @str: string to be printed
+ * @str: string to be printed the length
  */
 void _puts(char *str)
 {
 	int c;
 	int len;
+
 	len = _strlen(str);
 
 	for (c = 0; c <= len; c++)
@@ -20,15 +21,21 @@ void _puts(char *str)
 	}
 	_putchar('\n');
 }
+
+/**
+ * _strlen - prints length of a string
+ * @s: Pointer ref to the string
+ * Return: string length
+ */
 int _strlen(char *s)
 {
-        int len = 0;
+	int len = 0;
 
-        while (*s != '\0')
-        {
-                len++;
-                s++;
-        }
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
 
-        return (len);
+	return (len);
 }
