58e4062d9993f16b3e377a4523d38495672258fe
BigGuy <austinmuthoni0101@gmail.com>
2023-09-21 05:22:15 -0700

    cap_string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,27 +2,34 @@
 #include <stdio.h>
 
 /**
- *
+ * cap_string - converts the first letter of each word to cap
+ * Description: it takes a string and converts every letter after a space 
+ * or period or escape character into a cap letter
+ * @str: string to be capitalized
+ * Return: the string
  */
 char *cap_string(char *str)
 {
 	char *org_str = str;
 	int i;
-	
+
 	for (i = 0; str[i] != '\0'; i++)
 	{
+		if (str[i] == '\t' || str[i] == '\n' || str[i] == '\r' || str[i] == '\"' || str[i] == '\'')
+		{
+			if (str[i + 1] >= 97 && str[i + 1] <= 122)
+			{
+				str[i + 1] -= 32;
+			} 
+		}
+
+
 		if (str[i] == '.' || str[i] == ' ' || (str[i] >= 48 && str[i] <= 57))
 		{
 			if (str[i + 1] >= 97 && str[i + 1] <= 122)
 			{
 				str[i + 1] -= 32;
-			}
-		}
-		else if (str[i] == '\t' || str[i] == '\n')
-		{
-			if (str[i + 1] >= 97 && str[i + 1] <= 122)
-			{
-				str[i + 1] -= 32;
+			
 			}
 		}
 	}
