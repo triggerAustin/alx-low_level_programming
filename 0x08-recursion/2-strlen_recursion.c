99400d01cff2e3f0ac47ef245c409aafae00a7ad
BigGuy <austinmuthoni0101@gmail.com>
2023-09-27 05:06:05 -0700

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - prints the length of a string recursively
+ * @s: string 
+ * Description: each recursive call adds the output of the fn to i
+ * Return: value of i 
+ */
+int _strlen_recursion(char *s)
+{
+	int i;
+
+	i = 0;
+
+	if (*s != '\0')
+	{
+		i++;
+		return (i + _strlen_recursion(s + 1));
+	}
+	else
+	{
+		return (i);
+	}
+
+	return (i);
+}
