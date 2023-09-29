a2f167be6043e869a5958e000e98c32f2fe4f104
BigGuy <austinmuthoni0101@gmail.com>
2023-09-29 03:52:20 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints number of args in a program
+ * @argc: argument count
+ * @argv: argument vector
+ */
+int main(int argc, char *argv[])
+{
+	unsigned int i, j;
+	int count;
+
+	j = argc;
+
+	for (i = 0; i < j && *argv[i] != '\0'; i++)
+	{
+		count++;	
+	}
+	printf("%d\n", count - 1);
+	return (0);
+}
