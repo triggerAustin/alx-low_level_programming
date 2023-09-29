c2133b32eed295c27df3d4d27f755b153cf3006f
BigGuy <austinmuthoni0101@gmail.com>
2023-09-29 03:58:12 -0700

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints all characters in a command
+ * @argc: argument count
+ * @argv: argument vectors
+ * Return: 0 if successful
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 1; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
