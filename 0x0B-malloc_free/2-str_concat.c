175c29eb3170d773c6987a6252a9a503904476c4
BigGuy <austinmuthoni0101@gmail.com>
2023-10-03 05:22:01 -0700

    2-str_concat.c

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,75 @@
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * concate - concatanates two strings
+ * @str1: first string
+ * @str2: second string
+ * @i: length of first string
+ * @j: length of second string
+ * Return: concatanated string
+ */
+char *concate(char *str1, char *str2, int i, int j)
+{
+	int k, l;
+	char *concate_str;
+
+	concate_str = malloc((i + j + 1) * sizeof(char));
+
+	if (concate_str == NULL)
+	{
+		return (NULL);
+	}
+	for (k = 0; k < i; k++)
+	{
+		concate_str[k] = str1[k];
+	}
+
+	for (l = 0; l < j; l++)
+	{
+		concate_str[i + l] = str2[l];
+	}
+	concate_str[i + j] = '\0';
+
+	return (concate_str);
+}
+
+/**
+ * str_concat - concatanates two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: pointer to concate string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int len, len2;
+	char *org_s2, *org_s1;
+	char *con_str;
+
+	org_s1 = s1;
+	org_s2 = s2;
+	len = 0;
+	len2 = 0;
+
+	while (*s2 != '\0')
+	{
+		len++;
+		s2++;
+	}
+
+	while (*s1 != '\0')
+	{
+		len2++;
+		s1++;
+	}
+
+	con_str = malloc((len + len2 + 1) * sizeof(char));
+
+	if (con_str == NULL)
+	{
+		return (NULL);
+	}
+	con_str = concate(org_s1, org_s2, len2, len);
+
+	return (con_str);
+}
