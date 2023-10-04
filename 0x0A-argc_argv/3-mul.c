b5771bfd1050a7edd89cc6a04a79e040c0b9b95d
BigGuy <austinmuthoni0101@gmail.com>
2023-10-04 00:34:17 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,17 +1,42 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <stdlib.h>
 /**
- *
+ * main - multiplies two arguments 
+ * @argv: argument vector
+ * @argc: argument count
+ * Return: 1 if error 0 if success
  */
 int main(int argc, char *argv[])
 {
-	if (argc == 2 && isdigit(argv[1]) && isdigit(argv[2]))
+	int i;
+
+	if (argc == 3)
 	{
-		printf("%d\n", (*argv[1]) * (*argv[2]));
+		for (i = 0; argv[1][i] != '\0'; i++)
+		{
+			if (!isdigit(argv[1][i]))
+			{
+				printf("ERROR\n");
+				return (1);
+			}
+		}
+		for (i = 0; argv[2][i] != '\0'; i++)
+		{
+			if (!isdigit(argv[2][i]))
+			{
+				printf("ERROR\n");
+				return (1);
+			}
+		}
 	}
 	else
 	{
 		printf("ERROR\n");
+		return (1);
 	}
+
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+
 	return (0);
 }
