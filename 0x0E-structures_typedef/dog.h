019cf48cf6c625f87acc7cde378cdf22bff574eb
BigGuy <austinmuthoni0101@gmail.com>
2023-10-09 10:24:31 -0700

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,14 +6,14 @@
  * @name: name of dog
  * @age: age of dog
  * @owner: owner of dog
- */
+ *
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-
+*/
 /**
  * struct dog_t - struct for a dog
  * @name: name for dog
