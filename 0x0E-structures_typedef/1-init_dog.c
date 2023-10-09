f14b1a67dd9d9a199d46429299d1ccff34f9ffb2
BigGuy <austinmuthoni0101@gmail.com>
2023-10-09 10:36:35 -0700

    maloc handling

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -15,11 +15,12 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 
 	if (d != NULL)
 	{
-		if (d->name != NULL || d->age >= 0 || d->owner != NULL)
-		{
-			d->name = name;
-			d->age = age;
-			d->owner = owner;
-		}
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+	else
+	{
+		exit(1);
 	}
 }
