cbd444a3006889eced44abefba83c276cf36ecb5
BigGuy <austinmuthoni0101@gmail.com>
2023-10-09 10:19:20 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,5 +13,19 @@ struct dog
 	float age;
 	char *owner;
 };
+
+/**
+ * struct dog_t - struct for a dog
+ * @name: name for dog
+ * @age: age for dog
+ * @owner: name of owner
+ */
+struct dog_t
+{
+	char *name;
+	float age;
+	char *owner;
+};
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 #endif
