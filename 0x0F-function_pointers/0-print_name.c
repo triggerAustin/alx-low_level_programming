fb1b30095d07d72b8eb12253031778d300c854dc
BigGuy <austinmuthoni0101@gmail.com>
2023-10-11 08:13:41 -0700

    edited

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -9,9 +9,10 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	char *Name;
-
-	Name = malloc(sizeof(name));
-	Name = name;
-	f(Name);
+	int i;
+	
+	if (name != NULL && f != NULL)
+	{
+		f(name);
+	}
 }
