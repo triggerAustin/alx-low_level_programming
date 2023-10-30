4a0ed9830759a052dc0e4fcaa53ad21e162d7634
Austin <austinmuthoni0101@gmail.com>
2023-10-30 06:07:57 -0700

    edited for null file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 
 	length = 0;
 	if (text_content == NULL)
-		return (-1);
+		text_content = "";
 	while (text_content[length] != '\0')
 	{
 		length ++;
