9a98e88fd201221a3192cc69e9dfe5c7db88ad8c
Austin <austinmuthoni0101@gmail.com>
2023-10-30 06:06:36 -0700

    done task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * create_file - function that creates file
+ * @filename: pointer to filename to be created of char const type
+ * @text_content: string to be written into file
+ * Return: always successfull
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, fdwrite, length;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	if (fd == -1)
+		return (-1);
+
+	length = 0;
+	if (text_content == NULL)
+		return (-1);
+	while (text_content[length] != '\0')
+	{
+		length ++;
+	}
+
+	fdwrite = write(fd, text_content,length);
+	if (fdwrite == -1)
+		return (-1);
+
+	close(fd);
+	return (-1);
+}
