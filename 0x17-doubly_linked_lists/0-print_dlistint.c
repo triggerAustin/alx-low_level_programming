22e5dcc5799e753710c6eea56ba3c15e5467062d
Austin <trigger_austin00@proton.me>
2024-01-27 02:49:19 -0800

    readme

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -2,8 +2,10 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	const dlistint_t *current = h;
-	size_t i = 1;
+	size_t i = 0;
 
+	if (h == NULL)
+		return i;
 	while (current != NULL)
 	{
 		printf("%d\n", current->n);
