6460537f907329f2b83e46ba299fcea61c9fb55d
Austin <trigger_austin00@proton.me>
2024-01-27 02:54:20 -0800

    readme

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,11 +1,18 @@
 #include "lists.h"
+
+/**
+ * print_dlistint - prints elements of the list
+ * @h: pointer to head of list
+ * Return: number of elements in the dlist
+ *
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
 	const dlistint_t *current = h;
 	size_t i = 0;
 
 	if (h == NULL)
-		return i;
+		return (i);
 	while (current != NULL)
 	{
 		printf("%d\n", current->n);
