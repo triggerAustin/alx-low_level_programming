f453ce5921e551099769a5ae9e2dfd16f95bf1ed
Void <trigger_austin00@proton.me>
2024-05-04 14:45:02 +0300

    done

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -19,7 +19,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (table == NULL)
 		return (NULL);
 
-	array = malloc(sizeof(hash_node_t));
+	array = malloc(sizeof(hash_node_t) * size);
 	if (array == NULL)
 		return (NULL);
 
