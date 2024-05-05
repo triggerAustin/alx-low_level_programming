9a1cec0a65c20b79979fe5be12351605b53f05b7
Void <trigger_austin00@proton.me>
2024-05-06 00:51:35 +0300

    print fn for hash

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -8,25 +8,21 @@ void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i;
 	hash_node_t *p;
+	char *sep;
 
 	if (!ht)
 		return;
 
 	printf("{");
+	sep = ""
 	for (i = 0; ht && i < ht->size; i++)
 	{
 		p = ht->array[i];
 		while (p)
 		{
-			if (i == (ht->size - 1))
-			{
-				printf("%ld'%s': '%s'", i, p->key, p->value);
-			}
-			else
-			{
-				printf("%ld'%s': '%s', ", i, p->key, p->value);
-			}
+			printf("%s'%s': '%s'", sep, p->key, p->value);
 			p = p->next;
+			sep = ", "
 		}
 	}
 	printf("}\n");
