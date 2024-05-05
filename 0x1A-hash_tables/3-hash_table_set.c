59d349d9adf5ade7b7ca913716fc47dd1c15b302
Void <trigger_austin00@proton.me>
2024-05-05 15:26:05 +0300

    done

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,38 +13,24 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 
 	index = key_index((const unsigned char *) key, ht->size);
-	value_cp = strdup(value);
 	if (value == NULL)
 		return (0);
 
-	/* check if key exists*/
 	for (i = index; ht->array[i]; i++)
 	{
-		/* add value to array if key exists*/
 		if (strcmp(ht->array[i]->key, key) == 0)
 		{
 			free(ht->array[i]->value);
-			ht->array[i]->value = value_cp;
+			ht->array[i]->value = strdup(value);
 			return (1);
 		}
 	}
 	
-	/*add new node with node->value as value*/
 	new_hash_node = malloc(sizeof(hash_node_t));
 	if (!new_hash_node)
-	{
-		free(value_cp);
 		return (0);
-	}
 
-	new_hash_node->key = strdup(key);
-	if (!new_hash_node->key)
-	{
-		free(new_hash_node);
-		return(0);
-	}
-	
-	new_hash_node->value = value_cp;
+	new_hash_node->value = strdup(value);
 	new_hash_node->next = ht->array[index];
 	ht->array[index] = new_hash_node;
 	return (1);
