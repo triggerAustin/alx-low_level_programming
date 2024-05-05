8933fb8e895c9502d94970ca823609c01338a0ba
Void <trigger_austin00@proton.me>
2024-05-05 15:47:25 +0300

    done

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,7 +1,12 @@
 #include "hash_tables.h"
 
 /**
+ *  hash_table_set -This adds an element to the hash table
+ *  @ht: The hash table
+ *  @key: The key of the new element
+ *  @value: The value of the new element
  *
+ *  Return: 1 on success, 0 on failure
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
@@ -24,7 +29,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			return (1);
 		}
 	}
-	
+
 	new_hash_node = malloc(sizeof(hash_node_t));
 	if (!new_hash_node)
 		return (0);
