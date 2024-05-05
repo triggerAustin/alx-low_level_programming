3bb9265acc2a05a0a7e017e0b632d0a2f4108403
Void <trigger_austin00@proton.me>
2024-05-05 14:09:34 +0300

    to add a key value pair to hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -22,7 +22,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		if (strcmp(ht->array[i]->key, key) == 0)
 		{
 			free(ht->array[i]->value);
-			ht->array[i]->value = strdup(value);
+			ht->array[i]->value = NULL;
+			ht->array[i]->value = value_cp;
 			return (1);
 		}
 	}
