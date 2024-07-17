8ac2a7b074b8e539684b77861342e04de5e68857
penality <trigger_austin00@proton.me>
2024-07-17 10:16:44 +0300

    binary search algo: include math library

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -14,7 +14,7 @@ int binary_search(int *array, size_t size, int value)
 {
 	int L = 0;
 	int R = size - 1;
-	int m;
+	int m, i;
 
 	if (!array)
 		return (-1);
@@ -23,6 +23,8 @@ int binary_search(int *array, size_t size, int value)
 	{
 		m  = floor((L + R) / 2);
 
+		for (i = L; i <= R; L++)
+			printf("Searching in array: %d ", array[i])
 		if (array[m] < value)
 			L = (m + 1);
 		else if (array[m] > value)
